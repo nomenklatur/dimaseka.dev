73f5494a3caf2c04d6e0e5de8dc652d5a58dead1
Dimas <dimasekaputra2000@gmail.com>
2025-03-09 13:46:26 +0700

    feat: github and linkedin hrefs

--- src/app/components/landing_page/Hero.tsx
+++ src/app/components/landing_page/Hero.tsx
@@ -1,9 +1,10 @@
+"use client"
 import { Typewriter } from "react-simple-typewriter"
 export default function Hero() {
     return (
         <>
             <div className='w-screen h-screen flex flex-col justify-center items-center'>
-                <div className='text-[25px] md:text-[75px] font-bold text-center'>
+                <div className='text-[25px] md:text-[75px] font-bold text-center mb-4'>
                     Hello, I'm Dimas.
                     <div>
                     I am a
@@ -19,7 +20,21 @@ export default function Hero() {
                     </span>
                     </div>
                 </div>
-                <div className='mt-5 font-figtree'>
+                <div className="flex gap-4">
+                    <a href="https://github.com/nomenklatur" target="_blank" className="flex gap-1 font-figtree text-sm items-center md:text-2xl">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M12 2A10 10 0 0 0 2 12c0 4.42 2.87 8.17 6.84 9.5c.5.08.66-.23.66-.5v-1.69c-2.77.6-3.36-1.34-3.36-1.34c-.46-1.16-1.11-1.47-1.11-1.47c-.91-.62.07-.6.07-.6c1 .07 1.53 1.03 1.53 1.03c.87 1.52 2.34 1.07 2.91.83c.09-.65.35-1.09.63-1.34c-2.22-.25-4.55-1.11-4.55-4.92c0-1.11.38-2 1.03-2.71c-.1-.25-.45-1.29.1-2.64c0 0 .84-.27 2.75 1.02c.79-.22 1.65-.33 2.5-.33s1.71.11 2.5.33c1.91-1.29 2.75-1.02 2.75-1.02c.55 1.35.2 2.39.1 2.64c.65.71 1.03 1.6 1.03 2.71c0 3.82-2.34 4.66-4.57 4.91c.36.31.69.92.69 1.85V21c0 .27.16.59.67.5C19.14 20.16 22 16.42 22 12A10 10 0 0 0 12 2"/>
+                        </svg>
+                        Github
+                    </a>
+                    <a href="https://www.linkedin.com/in/masdimasekaputra/" target="_blank" className="flex gap-1 font-figtree text-sm items-center md:text-2xl">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M18.72 4H5.37A1.31 1.31 0 0 0 4 5.25v13.38A1.41 1.41 0 0 0 5.37 20h13.35A1.34 1.34 0 0 0 20 18.63V5.25A1.23 1.23 0 0 0 18.72 4M9 17.34H6.67v-7.13H9ZM7.89 9.13A1.18 1.18 0 0 1 6.67 7.9a1.18 1.18 0 0 1 1.24-1.23A1.18 1.18 0 0 1 9.13 7.9a1.18 1.18 0 0 1-1.24 1.23m9.45 8.21H15v-3.9c0-.93-.33-1.57-1.16-1.57a1.25 1.25 0 0 0-1.17.84a1.4 1.4 0 0 0-.08.57v4.06h-2.3v-7.13h2.3v1a2.32 2.32 0 0 1 2.1-1.21c1.51 0 2.65 1 2.65 3.13Z"/>
+                    </svg>
+                        LinkedIn
+                    </a>
+                </div>
+                <div className='mt-5 font-figtree text-xs md:text-xl'>
                     Stay tuned for more updates!
                 </div>
             </div>
