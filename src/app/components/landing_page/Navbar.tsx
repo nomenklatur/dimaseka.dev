8ca52426e2916ebd44ed10dbe8e498d6672c0c91
Dimas <dimasekaputra2000@gmail.com>
2025-03-09 11:36:39 +0700

    feat: download resume

--- src/app/components/landing_page/Navbar.tsx
+++ src/app/components/landing_page/Navbar.tsx
@@ -17,10 +17,10 @@ function Navbar() {
                     </li>
                 </ul>
 
-                <button className="bg-yellow-400 font-semibold px-2 py-1 md:px-2 md:py-1 lg:px-4 lg:py-2 rounded-md text-sm flex gap-1 items-center shadow-lg">
+                <a href="/files/Dimas Eka Putra - Resume.pdf" download="Dimas Eka Putra - Resume.pdf" className="bg-yellow-400 font-semibold cursor-pointer px-2 py-1 md:px-2 md:py-1 lg:px-4 lg:py-2 rounded-md text-sm flex gap-1 items-center shadow-lg">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill="currentColor" d="m12 15.577l-3.539-3.538l.708-.72L11.5 13.65V5h1v8.65l2.33-2.33l.709.719zM6.616 19q-.691 0-1.153-.462T5 17.384v-2.423h1v2.423q0 .231.192.424t.423.192h10.77q.23 0 .423-.192t.192-.424v-2.423h1v2.423q0 .691-.462 1.153T17.384 19z"/></svg>
                     Resume
-                </button>
+                </a>
             </nav>
         </>
     );
