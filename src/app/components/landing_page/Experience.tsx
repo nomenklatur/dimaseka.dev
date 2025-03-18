ab93b0934ff81aacc45d7853ca787d53ca7bcb87
Dimas <dimasekaputra2000@gmail.com>
2025-03-18 07:53:56 +0700

    Feat: setup experience section

---
+++ src/app/components/landing_page/Experience.tsx
@@ -0,0 +1,11 @@
+"use client"
+export default function Experience() {
+    return (
+        <>
+            <div className="w-screen h-screen flex flex-col justify-center items-center bg-red-400">
+                Code that performs. Solutions that scale. Results you can quantify.
+            </div>
+        
+        </>
+    );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 "use client"
 import Navbar from "./components/landing_page/Navbar";
 import Hero from "./components/landing_page/Hero";
+import Experience from "./components/landing_page/Experience";
 
 export default function Home() {
   return (
     <>
       <Navbar/>
       <Hero />
+      <Experience />
     </>
    );
 }
