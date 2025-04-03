06b43d2f2571656c5742c5f572e3b259dd76aaa0
Dimas <dimasekaputra2000@gmail.com>
2025-04-03 12:18:50 +0700

    feat: add telegram contact on hero section

--- src/app/components/landing_page/Hero.tsx
+++ src/app/components/landing_page/Hero.tsx
@@ -33,6 +33,12 @@ export default function Hero() {
                     </svg>
                         LinkedIn
                     </a>
+                    <a href="https://t.me/masstah" target="_blank" className="flex gap-1 font-figtree text-sm items-center md:text-2xl">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" viewBox="0 0 24 24">
+                        <path d="M9.78 18.65l.28-4.23l7.68-6.92c.34-.31-.07-.46-.52-.19L7.74 13.3L3.64 12c-.88-.25-.89-.86.2-1.3l15.97-6.16c.73-.33 1.43.18 1.15 1.3l-2.72 12.81c-.19.91-.74 1.13-1.5.71L12.6 16.3l-1.99 1.93c-.23.23-.42.42-.83.42z" fill="currentColor"/>
+                    </svg>
+                        Telegram
+                    </a>
                 </div>
                 <div className="mt-6 md:mt-8">
                     <a href="#Experience" className="bg-gray-800 cursor-pointer text-white px-3 py-1 md:px-2 md:py-1 lg:px-4 lg:py-2 rounded-md text-sm shadow-lg">Get to know me!</a>
