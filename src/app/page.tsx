4d1a749d8adeca6ed3d31ff91eb96e0b3cdc1176
Dimas <dimasekaputra2000@gmail.com>
2025-07-02 12:09:51 +0700

    feat: setup chat interface

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import Hero from "./components/landing_page/Hero";
 import Experience from "./components/landing_page/Experience";
 import Projects from "./components/landing_page/Projects";
 import Footer from "./components/landing_page/Footer";
+import FloatingButton from "./components/landing_page/FloatingButton";
 
 export default function Home() {
   return (
     <div className="h-fit overflow-y-scroll snap-y snap-proximity block">
       <Navbar/>
       <Hero />
+      <FloatingButton />
       <Projects />
       <Experience />
       <Footer />
