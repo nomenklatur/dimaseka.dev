f516c49ea509d2790ad7fe287bab23c964ce8381
Dimas <dimasekaputra2000@gmail.com>
2025-07-03 19:20:15 +0700

    chore: code refactor

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 "use client"
-import Navbar from "./components/landing_page/Navbar";
-import Hero from "./components/landing_page/Hero";
-import Experience from "./components/landing_page/Experience";
-import Projects from "./components/landing_page/Projects";
-import Footer from "./components/landing_page/Footer";
-import FloatingButton from "./components/landing_page/FloatingButton";
+import Navbar from "../components/custom/landing_page/Navbar";
+import Hero from "../components/custom/landing_page/Hero";
+import Experience from "../components/custom/landing_page/Experience";
+import Projects from "../components/custom/landing_page/Projects";
+import Footer from "../components/custom/landing_page/Footer";
+import FloatingButton from "../components/custom/landing_page/FloatingButton";
 
 export default function Home() {
   return (
