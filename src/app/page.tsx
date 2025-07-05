996a9c52ab6499ae9308f014cb9c905412b5a674
Dimas <dimasekaputra2000@gmail.com>
2025-07-05 11:33:11 +0700

    feat: freelance platforms section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Experience from "../components/custom/landing_page/Experience";
 import Projects from "../components/custom/landing_page/Projects";
 import Footer from "../components/custom/landing_page/Footer";
 import FloatingButton from "../components/custom/landing_page/FloatingButton";
+import FreelancePlatforms from "@/components/custom/landing_page/FreelancePlatforms";
 
 export default function Home() {
   return (
@@ -14,6 +15,7 @@ export default function Home() {
       <FloatingButton />
       <Projects />
       <Experience />
+      <FreelancePlatforms />
       <Footer />
     </div>
    );
